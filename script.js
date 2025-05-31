8d0c9e4e9ed69787295fbe1dee99f8749111a81a
Oliver Villalobos <71286508+olivervillalobos@users.noreply.github.com>
2025-05-31 08:05:45 -0700

    Work on some event handlers and some css styling

--- script.js
+++ script.js
@@ -8,20 +8,56 @@ const updateDate = () => {
 updateDate();
 const listTitle = document.getElementById("list-title");
 const tasksContainer = document.getElementById("tasks-container");
+const newTaskTemplate = `
+    <div class="new-task">
+        <div>
+            <img id="add-task-icon" src="./images/plus.png">
+        </div>
+        <div>
+            <p id="new-text">Add new task</p>
+        </div>
+        <div>
+            <img id="new-task-cat" src="./images/cat.png" alt="cat waiting for you to add a task">
+        </div>
+    </div>
+    `;
 const taskTitle = JSON.parse(localStorage.getItem("title")) || "";
 const taskData = JSON.parse(localStorage.getItem("tasks")) || [];
 
-tasksContainer.innerHTML += 
-`
-<div class="new-task">
-    <div>
-        <img id="add-task-icon" src="./images/plus.png">
+const clearContainer = () => {
+    tasksContainer.innerHTML = "";
+}
+
+const updateTitle = () =>{
+    listTitle.value = taskTitle;
+}
+updateTitle();
+
+const displayTasks = (taskReturnedData) => {
+    tasksContainer.innerHTML += newTaskTemplate;
+    
+};
+displayTasks(taskData);
+
+const newTask = document.querySelector(".new-task");
+
+const addNewTaskSpace = () => {
+    const newTaskSpace = 
+    `
+    <div class="task">
+        <div>
+            <img class="task-box" src="./images/box.png">
+        </div>
+        <div class="my">
+            <input type="text" placeholder="Write here" id="task-list" name="task-list" class="task-input">
+        </div>
+        <div>
+            <img class="task-trash" src="./images/bin.png">
+        </div>
     </div>
-    <div>
-        <p id="new-text">Add new task</p>
-    </div>
-    <div>
-        <img id="new-task-cat" src="./images/cat.png" alt="cat waiting for you to add a task">
-    </div>
-</div>
-`;
+    `;
+    clearContainer();
+    tasksContainer.innerHTML = newTaskSpace + newTaskTemplate;
+}
+addNewTaskSpace();
+newTask.addEventListener('click', addNewTaskSpace);
