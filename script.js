9d669badcc8b1dda84d3cd2e12c20c5e638e11f7
Oliver Villalobos <71286508+olivervillalobos@users.noreply.github.com>
2025-06-02 07:57:35 -0700

    I added dome functionallity on adding one single task, but fail to add more than one lol

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const updateDate = () => {
-    const dateField = document.getElementById("date-list");
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    const date = new Date();
-    dateField.textContent = `${days[date.getDay()]} ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  const dateField = document.getElementById("date-list");
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const date = new Date();
+  dateField.textContent = `${days[date.getDay()]} ${
+    months[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()}`;
 };
-updateDate();
 const listTitle = document.getElementById("list-title");
 const tasksContainer = document.getElementById("tasks-container");
 const newTaskTemplate = `
@@ -23,41 +45,89 @@ const newTaskTemplate = `
     `;
 const taskTitle = JSON.parse(localStorage.getItem("title")) || "";
 const taskData = JSON.parse(localStorage.getItem("tasks")) || [];
-
 const clearContainer = () => {
-    tasksContainer.innerHTML = "";
-}
-
-const updateTitle = () =>{
-    listTitle.value = taskTitle;
-}
-updateTitle();
-
-const displayTasks = (taskReturnedData) => {
-    tasksContainer.innerHTML += newTaskTemplate;
-    
+  tasksContainer.innerHTML = "";
 };
-displayTasks(taskData);
 
-const newTask = document.querySelector(".new-task");
+const updateTitle = () => {
+  listTitle.value = taskTitle;
+};
+
+let tempSaver;
+const displayTasks = (taskReturnedData) => {
+  clearContainer();
+
+  taskReturnedData.forEach((element) => {
+    tasksContainer.innerHTML += `
+        <div class="task ${element.state}">
+            <div>
+                <img class="task-box" src="./images/box.png">
+            </div>
+            <div class="my">
+                <input type="text" placeholder="Write here" id="task-list${element.id}" name="task-list" class="task-input" value=${element.value}>
+            </div>
+            <div>
+                <img class="task-trash" src="./images/bin.png">
+            </div>
+        </div>
+        `;
+  });
+
+  tempSaver = tasksContainer.innerHTML;
+
+  tasksContainer.innerHTML += newTaskTemplate;
+
+  const newTask = document.querySelector(".new-task");
+  if (newTask) {
+    newTask.addEventListener("click", function () {
+      console.log("A ver que show");
+      addNewTaskSpace();
+    });
+  }
+};
 
 const addNewTaskSpace = () => {
-    const newTaskSpace = 
+  let state = false;
+  let id = crypto.randomUUID(); // ✅ generates unique ID
+  let value = "";
+
+  tasksContainer.innerHTML =
+    tempSaver +
     `
-    <div class="task">
-        <div>
-            <img class="task-box" src="./images/box.png">
+        <div class="task ${state}">
+            <div>
+                <img class="task-box" src="./images/box.png">
+            </div>
+            <div class="my">
+                <input type="text" placeholder="Write here" id="task-list${id}" name="task-list" class="task-input" value=${value}>
+            </div>
+            <div>
+                <img class="task-trash" src="./images/bin.png">
+            </div>
         </div>
-        <div class="my">
-            <input type="text" placeholder="Write here" id="task-list" name="task-list" class="task-input">
-        </div>
-        <div>
-            <img class="task-trash" src="./images/bin.png">
-        </div>
-    </div>
-    `;
-    clearContainer();
-    tasksContainer.innerHTML = newTaskSpace + newTaskTemplate;
-}
-addNewTaskSpace();
-newTask.addEventListener('click', addNewTaskSpace);
+    ` +
+    newTaskTemplate;
+
+  const input = document.getElementById(`task-list${id}`);
+  input.addEventListener("blur", function (e) {
+    value = e.target.value.trim();
+
+    if (value !== "") {
+      taskData.push({
+        id: id,
+        value: value,
+        state: state,
+      });
+      localStorage.setItem("tasks", JSON.stringify(taskData)); // Save it!
+    }
+
+    console.log(taskData);
+  });
+};
+
+
+window.addEventListener("load", function () {
+  updateDate();
+  updateTitle();
+  displayTasks(taskData);
+});
